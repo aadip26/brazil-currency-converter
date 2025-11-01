9db6084e63c33d65b2b8cf270c8618d27f5278cf
aadip26 <aadipawar4940@gmail.com>
2025-11-01 14:16:17 +0530

    Fix: use Render port variable

--- src/app.js
+++ src/app.js
@@ -16,9 +16,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", currencyRoutes);
 
 app.get("/", (req, res) => {
- res.sendFile(path.join(__dirname, "public", "index.html"));
-
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
